9a61187cbfd83008c416b9e2040a7f33cddf2394
zhaoyan <2292111016.com>
2022-07-10 22:18:47 +0800

    订单界面

---
+++ 前端/pages/myorders/myorders.js
@@ -0,0 +1,32 @@
+// pages/myreservation/myorders.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "phoneNum" : "15892222603",
+    "orders":[],
+    "orderNum" : 0
+  },
+
+  onLoad(options) {
+    var that = this;
+    //console.log(options);
+      
+    wx.request({
+      url: 'http://localhost:8080/order/app/order/'+this.data.phoneNum,
+      method:"GET",
+      success:function(res){
+        //console.log(res);
+        //console.log(res.data.data)
+        that.setData({
+          "orders":res.data.data,
+          "orderNum":res.data.data.length
+        })
+      }
+    })
+  },
+
+  
+})
