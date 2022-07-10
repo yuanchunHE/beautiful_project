f826717f9e6badd564b0cbfd5b063192d8831e74
Updraft095 <109008917+Updraft095@users.noreply.github.com>
2022-07-10 23:35:43 +0800

    myorders到orderdetail跳转 myorder连接数据库
    myorders到orderdetail跳转 myorder连接数据库

--- 前端/pages/myorders/myorders.js
+++ 前端/pages/myorders/myorders.js
@@ -12,14 +12,11 @@ Page({
 
   onLoad(options) {
     var that = this;
-    //console.log(options);
       
     wx.request({
       url: 'http://localhost:8080/order/app/order/'+this.data.phoneNum,
       method:"GET",
       success:function(res){
-        //console.log(res);
-        //console.log(res.data.data)
         that.setData({
           "orders":res.data.data,
           "orderNum":res.data.data.length
@@ -27,6 +24,12 @@ Page({
       }
     })
   },
-
+  toOrderDetail:function(options){
+    //console.log(options);
+    var usertell = options.currentTarget.dataset.usertell;
+    wx.navigateTo({
+      url: '/pages/orderdetails/orderdetails?usertell='+usertell,
+    })
+  },
   
 })
